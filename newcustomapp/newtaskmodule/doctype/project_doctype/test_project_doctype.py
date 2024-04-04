5ea07bac362ff570b6eef679907473880f03a876
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2024-04-04 15:55:59 +0530

    removing files

--- newcustomapp/newtaskmodule/doctype/project_doctype/test_project_doctype.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, priyanshu and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestProject_Doctype(FrappeTestCase):
-	pass
