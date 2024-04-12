f2302c51803ce9a82aa8315f78489f4ea5f05f45
Priyanshu Awasthi <priyanshu.awas011@gmail.com>
2024-04-12 11:05:54 +0530

    changes

--- newcustomapp/serversite/doctype/studentdetails/studentdetails.py
+++ newcustomapp/serversite/doctype/studentdetails/studentdetails.py
@@ -3,11 +3,17 @@
 
 import frappe # type: ignore
 from frappe.model.document import Document # type: ignore
-
 class StudentDetails(Document):
 	pass
-
-class StudentDetails(Document):
-    def validate(self):
-        if self.checked and self.first_name and self.last_name:
-            self.full_name = f"{self.first_name} {self.last_name}" 
+def update_full_name(doc, method):
+    if doc.first_name and doc.last_name:
+        full_name = doc.first_name + " " + doc.last_name
+        doc.full_name = full_name
+def register_student_details_hooks():
+    frappe.db.append("DocType", {
+        "doctype": "DocEvent",
+        "ref_doctype": "Student Details",
+        "event": "before_save",
+        "method": "newcustomapp.student_details.update_full_name"
+    })
+register_student_details_hooks()
